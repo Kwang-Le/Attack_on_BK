e73f71d06b0b3784baabb9bda8f2558fe7e18622
DjangoLong <91949101+DjangoLong@users.noreply.github.com>
2021-10-14 10:49:08 +0700

    Add files via upload

---
+++ Enemy/SInh_vien.py
@@ -0,0 +1,22 @@
+import pygame
+import os
+from .enemy import Enemy
+
+width = 18
+height = 40
+imgs = []
+for i in range(0,2):
+    image_load = pygame.image.load(os.path.join("asset/Enemy/", "Sinhvien" + str(i) + ".png"))
+    # image_load = pygame.transform.flip(image_load, True, False)
+    imgs.append(pygame.transform.scale(image_load, (width, height)))
+class Sinh_vien(Enemy):
+
+    def __init__(self, waypoints, win):
+        super().__init__(waypoints, win)
+        self.imgs = imgs[:]
+        self.max_health = 1000
+        self.current_health = self.max_health
+        self.speed = self.init_speed * 3
+        self.money = 1
+        self.width = 18
+        self.height = 40
